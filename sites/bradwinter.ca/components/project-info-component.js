d87d910379f50d7639fea902e0c0b051f2dd916c
brad winter <braddjwinter@gmail.com>
2017-09-16 16:43:35 -0700

    trust as html

--- sites/bradwinter.ca/components/project-info-component.js
+++ sites/bradwinter.ca/components/project-info-component.js
@@ -2,11 +2,15 @@ angular.module('brad')
   .component('projectInfo', {
 
     templateUrl: 'components/project-info.html',
-    controller: function ($scope, $element, $attrs, $state, projects) {
+    controller: function ($scope, $element, $attrs, $state, $sce, projects) {
 
       var project = _.get($state, 'current.data.project');
 
       $scope.project = project;
+
+      $scope.sanitize = function(input){
+        return $sce.trustAsHtml(input);
+      }
 
       $scope.nextPageSlug = function () {
         var i = _.findIndex(projects, {slug: project.slug});
