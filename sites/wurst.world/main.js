34fd8182421615a7dced3fc3e44d99825f554895
bradwbradw <bradwbradw@xyz.gs>
2018-04-01 15:22:48 -0700

    vabf.html

--- sites/wurst.world/main.js
+++ sites/wurst.world/main.js
@@ -1,101 +1,100 @@
 var WW = angular.module('ww', ['ui.router', 'simple-sprite']);
 
 WW.controller('mainController', function ($scope, $state) {
-  console.log('state', $state.current);
+    console.log('state', $state.current);
 
-  var bodyClass = function () {
-    return $state.current.name.split('.');
-  };
-  var showNav = function () {
-    return !$state.is('home') || !$state.abstract;
-  };
+    var bodyClass = function () {
+        return $state.current.name.split('.');
+    };
+    var showNav = function () {
+        return !$state.is('home') || !$state.abstract;
+    };
 
-  $scope.bodyClass = bodyClass;
-  $scope.showNav = showNav;
+    $scope.bodyClass = bodyClass;
+    $scope.showNav = showNav;
 });
 
 WW.config(function ($locationProvider, $stateProvider, $urlRouterProvider) {
 
-  $locationProvider.html5Mode(false);
+    $locationProvider.html5Mode(false);
 
-  var states = {
-    home: {
-      name: 'home',
-      url: '/',
-      templateUrl: 'views/home.html'
-    },
-    contact: {
-      name: 'contact',
-      url: '/contact',
-      templateUrl: 'views/contact.html'
-    },
-    shop: {
-      name: 'shop',
-      url: '/shop',
-      templateUrl: 'views/shop.html',
-      controller: function ($scope, $state) {
+    var states = {
+        home: {
+            name: 'home',
+            url: '/',
+            templateUrl: 'views/home.html'
+        },
+        contact: {
+            name: 'contact',
+            url: '/contact',
+            templateUrl: 'views/contact.html'
+        },
+        shop: {
+            name: 'shop',
+            url: '/shop',
+            templateUrl: 'views/shop.html',
+            controller: function ($scope, $state) {
 
-        $scope.animationStarted = false;
-        var stateWhenAnimationDone = null;
-        if ($state.current.name === 'shop') {
+                $scope.animationStarted = false;
+                var stateWhenAnimationDone = null;
+                if ($state.current.name === 'shop') {
 
-          $scope.wallStillStanding = true;
-          $scope.$on('wall destroyed', function () {
-            console.log('wall is destroyed');
-            $scope.wallStillStanding = false;
-            if(stateWhenAnimationDone){
-              $state.go(stateWhenAnimationDone);
+                    $scope.wallStillStanding = true;
+                    $scope.$on('wall destroyed', function () {
+                        console.log('wall is destroyed');
+                        $scope.wallStillStanding = false;
+                        if (stateWhenAnimationDone) {
+                            $state.go(stateWhenAnimationDone);
+                        }
+                    })
+                } else {
+
+                    $scope.wallStillStanding = false;
+
+                }
+
+
+                $scope.go = function (state) {
+                    console.log(state);
+                    if ($scope.wallStillStanding) {
+                        stateWhenAnimationDone = state;
+                        $scope.animationStarted = true;
+                    } else {
+                        $state.go(state);
+                    }
+                }
+
             }
-          })
-        } else {
-
-          $scope.wallStillStanding = false;
-
+        },
+        scrap: {
+            name: '/scrap',
+            url: '/scrap',
+            templateUrl: 'views/scrap.html'
         }
+    };
 
+    var shopStates = 'enamel-pins prints shirts tote-bags'.split(' ');
 
-
-          $scope.go = function (state) {
-            console.log(state);
-            if ($scope.wallStillStanding) {
-              stateWhenAnimationDone = state;
-              $scope.animationStarted = true;
-            } else {
-              $state.go(state);
-            }
-          }
-
-      }
-    },
-    scrap: {
-      name: '/scrap',
-      url: '/scrap',
-      templateUrl: 'views/scrap.html'
-    }
-  };
-
-  var shopStates = 'enamel-pins prints shirts tote-bags'.split(' ');
-
-  _.each(states, function (state) {
-    $stateProvider.state(state);
-  });
-
-  _.each(shopStates, function (name) {
-
-    $stateProvider.state({
-      name: 'shop.' + name,
-      url: '/' + name,
-      views: {
-        product: {
-          templateUrl: 'views/products/' + name + '.html',
-          controller: function ($scope) {
-          }
-        }
-      },
+    _.each(states, function (state) {
+        $stateProvider.state(state);
     });
-  });
 
-  $urlRouterProvider.otherwise('/');
+    _.each(shopStates, function (name) {
+
+        $stateProvider.state({
+            name: 'shop.' + name,
+            url: '/' + name,
+            views: {
+                product: {
+                    templateUrl: 'views/products/' + name + '.html',
+                    controller: function ($scope) {
+                    }
+                }
+            }
+        });
+    });
+
+    $urlRouterProvider.otherwise('/');
 
 
 });
