c6fb2a5bc503c7d6a0d9070a1b30680b72996996
brad winter <braddjwinter@gmail.com>
2018-08-25 15:22:27 -0700

    service worker boilerplate caching code

---
+++ sites/scramples.xyz.gs/scripts/service-worker.js
@@ -0,0 +1,67 @@
+var CACHE_NAME = 'scramples-cache-v1';
+var urlsToCache = [
+  '/',
+  '/style.css',
+  '/scripts/main.js'
+];
+console.log(urlsToCache);
+
+var serviceWorker = self;
+
+serviceWorker.addEventListener('install', function(event) {
+  // Perform install steps
+  // cache files...
+
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        console.log('Opened cache', cache);
+        return cache.addAll(urlsToCache)
+          .catch(e => {
+            console.log("error adding to cache",e);
+          });
+      })
+  );
+});
+
+serviceWorker.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        // Cache hit - return response
+        console.log("sw cache hit", event.request, response);
+        if (response) {
+          return response;
+        }
+
+        // IMPORTANT: Clone the request. A request is a stream and
+        // can only be consumed once. Since we are consuming this
+        // once by cache and once by the browser for fetch, we need
+        // to clone the response.
+        var fetchRequest = event.request.clone();
+
+        return fetch(fetchRequest).then(
+          function(response) {
+            // Check if we received a valid response
+            if(!response || response.status !== 200 || response.type !== 'basic') {
+              return response;
+            }
+
+            // IMPORTANT: Clone the response. A response is a stream
+            // and because we want the browser to consume the response
+            // as well as the cache consuming the response, we need
+            // to clone it so we have two streams.
+            var responseToCache = response.clone();
+
+            caches.open(CACHE_NAME)
+              .then(function(cache) {
+                console.log("sw cache put", event.request, responseToCache);
+                cache.put(event.request, responseToCache);
+              });
+
+            return response;
+          }
+        );
+      })
+    );
+});
